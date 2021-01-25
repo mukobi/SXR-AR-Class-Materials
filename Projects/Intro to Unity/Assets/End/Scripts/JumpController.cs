0e07af93596196cf8eb0305f277c055bce0a1ffc
Gabriel Mukobi <gabrielmukobi@gmail.com>
2021-01-24 17:38:56 -0800

    Create tutorial content

---
+++ Projects/Intro to Unity/Assets/End/Scripts/JumpController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    public float jumpForce = 10.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MyFunction();
+        }
+    }
+
+    void MyFunction()
+    {
+        Debug.Log("Jump");
+        rb.AddForce(Vector3.up * jumpForce);
+    }
+}
